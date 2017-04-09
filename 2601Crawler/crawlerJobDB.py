884a261a61b7b157372ddf43fc03624eeb34499e
Fan Zeng <fanzengau@gmail.com>
2017-04-10 01:06:00 +0800

    Fixed Some Bugs

--- 2601Crawler/crawlerJobDB.py
+++ 2601Crawler/crawlerJobDB.py
@@ -1,4 +1,5 @@
 import urllib2
+import socket
 import os
 import re
 import HTMLParser
@@ -29,7 +30,7 @@ def crawl(page_number):
             jdreq = urllib2.Request(jdurl[0])
             jdresponse = urllib2.urlopen(jdreq)
             jdhtml = jdresponse.read()
-            jdhtml = jdhtml.decode("utf-8")
+            # jdhtml = jdhtml.decode("utf-8")
 
             jobtitle = re.findall('<h1 itemprop="title" class="general-pos ad-y-auto-txt">(.*?)</h1>|<h1 class="general-pos ad-y-auto-txt2" itemprop="title">(.*?)</h1>', jdhtml, re.S)
 
@@ -37,9 +38,9 @@ def crawl(page_number):
             print company
             if company.__len__() > 0 and company[0].__len__() > 0:
                 company_info = re.findall(r'<a href="(.*?)" target="_blank" class="popupInvoker">(.*?)</a>', company[0][0], re.S)
-            if company_info.__len__()>0:
-                company_link, company_name = company_info[0][0], company_info[0][1]
-                print company_link, company_name
+                if company_info.__len__()>0:
+                    company_link, company_name = company_info[0][0], company_info[0][1]
+                    print company_link, company_name
             else:
                 if company.__len__() > 0 and company[0].__len__() > 0 and company[0][0].__len__() > 0:
                     company_name = company[0][0]
@@ -49,11 +50,13 @@ def crawl(page_number):
 
 
             with open('./html/openings.txt', 'a') as jdfile:
-                jdfile.write(parser.unescape(company_name.strip()) + ': \n\t')
-                if jobtitle[0][0].__len__() > 0:
-                    jdfile.write(parser.unescape(jobtitle[0][0].strip()) + '\n')
-                elif jobtitle[0][1].__len__() > 0:
-                    jdfile.write(parser.unescape(jobtitle[0][1].strip()) + '\n')
+                if 'company_name' in locals():
+                    jdfile.write(parser.unescape(company_name.strip()) + ': \n\t')
+                if jobtitle.__len__() >0 and jobtitle[0].__len__() > 0:
+                    if jobtitle[0][0].__len__() > 0:
+                        jdfile.write(parser.unescape(jobtitle[0][0].strip()) + '\n')
+                    elif jobtitle[0][1].__len__() > 0:
+                        jdfile.write(parser.unescape(jobtitle[0][1].strip()) + '\n')
                 jdfile.write('\n')
 
             jobrequire = re.findall(r'<div itemprop="responsibilities" class="jobad-primary-details col-xs-9">(.*?)</ul>', jdhtml, re.S)
@@ -63,7 +66,8 @@ def crawl(page_number):
 
 
 if __name__ == '__main__':
-    for page_number in range(1, 100):
+    socket.setdefaulttimeout(10)
+    for page_number in range(21, 22):
         print "crawling Page: ", page_number, "\n\n\n\n\n"
         gethtml(str(page_number))
         crawl(str(page_number))
