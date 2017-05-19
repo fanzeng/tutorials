70ea93320fd145b7b0cd00202a87973a58fbd145
Fan Zeng <commonzeng@gmail.com>
2017-05-20 00:16:57 +0800

    Added DisplayVideo.cpp

--- opencv_tutorials/opencv_cpp_tutorials/src/myrun.sh
+++ opencv_tutorials/opencv_cpp_tutorials/src/myrun.sh
@@ -1,3 +1,3 @@
-cp /media/fzeng/DATA/PycharmProjects/Practice/opencv_tutorials/opencv_cpp_tutorials/build/bin/$1 .
+cp /media/fzeng/Data/Codes/PycharmProjects/Practice/opencv_tutorials/opencv_cpp_tutorials/build/bin/$1 .
 chmod 755 $1
 ./$@
