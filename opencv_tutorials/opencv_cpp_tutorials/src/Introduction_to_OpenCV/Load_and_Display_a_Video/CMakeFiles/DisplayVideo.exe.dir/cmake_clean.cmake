388418ac14bea5545abfad82333bfaa3efeae597
Fan Zeng <fanzengau@gmail.com>
2017-06-01 16:17:25 +0800

    Added The Core Functionality

---
+++ opencv_tutorials/opencv_cpp_tutorials/src/Introduction_to_OpenCV/Load_and_Display_a_Video/CMakeFiles/DisplayVideo.exe.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/DisplayVideo.exe.dir/DisplayVideo.cpp.o"
+  "../../../bin/DisplayVideo.exe.pdb"
+  "../../../bin/DisplayVideo.exe"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/DisplayVideo.exe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
