9d535a902ad202a94a5863ef910ab9818aa91105
Fan Zeng <fanzengau@gmail.com>
2024-09-16 17:32:56 +1000

    Navigate between screens

--- xcode-tutorials/Scrumdinger/Scrumdinger/DetailView.swift
+++ xcode-tutorials/Scrumdinger/Scrumdinger/DetailView.swift
@@ -12,9 +12,11 @@ struct DetailView: View {
     var body: some View {
         List {
             Section(header: Text("Meeting info")) {
-                Label("Start meeting", systemImage: "timer")
-                    .font(.headline)
-                    .foregroundColor(.accentColor)
+                NavigationLink(destination: MeetingView()) {
+                    Label("Start meeting", systemImage: "timer")
+                        .font(.headline)
+                        .foregroundColor(.accentColor)
+                }
                 HStack {
                     Label("Length", systemImage: "clock")
                     Spacer()
@@ -39,6 +41,7 @@ struct DetailView: View {
             }
             
         }
+        .navigationTitle(scrum.title)
     }
 }
 
